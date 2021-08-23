de33b8f379ac980f359a5d7d96f010075626a370
Rameen Humayun <76857653+RameenH26@users.noreply.github.com>
2021-08-23 21:16:57 +0500

    Add files via upload

--- bottle.js
+++ bottle.js
@@ -1,10 +1,44 @@
+status = "";
+objects = [];
 function preload() {
- img = loadImage('https://www.gannett-cdn.com/presto/2020/04/02/USAT/6e94ea0e-a80e-4269-b780-afa8e4db93ff-yeti-rambler-water-bottle.jpg');
+ img = loadImage('bottle.png');
 }
 function setup() {
-canvas = createCanvas(550, 400);
-canvas.position(370, 125);
+canvas = createCanvas(500, 350);
+canvas.position(400, 150);
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById('status').innerHTML = "Status : Detecting";
 }
 function draw() {
-    image(img, 0, 0, 550, 400);
+    image(img, 0, 0, 500, 350);
+    if(status != "") {
+        for(x = 0; x < objects.length; x++) {
+            document.getElementById('status').innerHTML = "Status : Detected" ;
+            document.getElementById('objectNum').innerHTML = "Objects Detected : " + objects.length;
+            stroke('#00000');
+            strokeWeight(1);
+            percentage = floor(objects[x].confidence * 100);
+            text(objects[x].label + " " + percentage + "%" , objects[x].x, objects[x].y + 45);
+            noFill();
+            strokeWeight(3);
+            stroke("#557571");
+            rect(objects[x].x, objects[x].y + 50, objects[x].width, objects[x].height);
+        }
+    }
+}
+function home() {
+ location.href="index.html";
+}
+
+function modelLoaded() {
+    console.log("model loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, result) {
+    if(error) {
+        console.error();
+    } 
+        console.log(result);
+        objects = result;
 }
--- shoe.js
+++ shoe.js
@@ -1,10 +1,47 @@
+status = "";
+objects = [];
 function preload() {
     img = loadImage('https://5.imimg.com/data5/ANDROID/Default/2020/10/RW/EB/KX/113109586/product-jpeg-500x500.jpg');
-   }
-   function setup() {
-   canvas = createCanvas(550, 400);
-   canvas.position(370, 125);
-   }
-   function draw() {
-       image(img, 0, 0, 550, 400);
-   }
+}
+function setup() {
+   canvas = createCanvas(525, 400);
+   canvas.position(370, 150);
+
+   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+   document.getElementById('status').innerHTML = "Status : Detecting";
+}
+function draw() {
+       image(img, 0, 0, 525, 455);
+
+       if(status != "") {
+           for(x = 0; x < objects.length; x++) {
+               document.getElementById('status').innerHTML = "Status : Detected" ;
+               document.getElementById('objectNum').innerHTML = "Objects Detected : " + objects.length;
+               stroke('#00000');
+               strokeWeight(1);
+               percentage = floor(objects[x].confidence * 100);
+               text(objects[x].label + " " + percentage + "%" , objects[x].x, objects[x].y + 45);
+               noFill();
+               strokeWeight(3);
+               stroke("#557571");
+               rect(objects[x].x, objects[x].y + 50, objects[x].width, objects[x].height);
+           }
+       }
+}
+   
+function home() {
+    location.href="index.html";
+}
+
+function modelLoaded() {
+    console.log("model loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, result) {
+    if(error) {
+        console.error();
+    } 
+        console.log(result);
+        objects = result;
+}
